93d3f7260ca71f056e52645dcaf3c387cc9b0df4
fernandinho <lfrfernandinho@gmail.com>
2020-03-03 00:38:05 -0300

    segundo commit

---
+++ src/br/fai/projeto01/Main.java
@@ -0,0 +1,36 @@
+package br.fai.projeto01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		Main app = new Main();
+		app.start();
+	}
+
+	private void start() {
+
+		int idade = 15;
+		
+		System.out.println("A idade é de " + idade + " anos.");
+		
+		
+		idade += 5; //idade = idade + 5;
+		idade *= 2;
+		
+		System.out.println("Resultado 1: " + idade);
+		
+		idade -= 5;
+		
+		System.out.println("Resultado 2: " + idade);
+		
+		if(idade > 20) {
+			System.out.println("A idade é maior que 20");
+		} else {
+			System.out.println("A idade é menor que 20");
+		}
+		
+	}
+}
